ad046c1d42e25fd20279355ad0f8504e0bee328f
SSAFY <lsn5963@naver.com>
2025-07-23 10:02:57 +0900

    S13P11A504-32 fix: s3 메소드 명 수정

--- backend/src/main/java/com/ssafy/backend/common/exception/S3ExceptionHandler.java
+++ backend/src/main/java/com/ssafy/backend/common/exception/S3ExceptionHandler.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class S3ExceptionHandler {
     @ExceptionHandler(S3UploadFailedException.class)
-    public CommonResponse<ErrorBody> UserPlanExistException(S3UploadFailedException e, HttpServletRequest request) {
+    public CommonResponse<ErrorBody> S3UploadFailedException(S3UploadFailedException e, HttpServletRequest request) {
         log.warn("S3-001> 요청 URI: " + request.getRequestURI() + ", 에러 메세지: " + e.getMessage());
         return new CommonResponse<>(new ErrorBody("S3-001", "S3 업로드 실패하였습니다."),
-                HttpStatus.BAD_REQUEST);
+                HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }

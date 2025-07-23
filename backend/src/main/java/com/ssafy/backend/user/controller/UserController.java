74671ea832d41eb48cdecf26518b9562a00f5f6d
SSAFY <lsn5963@naver.com>
2025-07-23 12:52:58 +0900

    S13P11A504-28 feat: 유저 프로필 수정 API 추가

---
+++ backend/src/main/java/com/ssafy/backend/user/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.ssafy.backend.user.controller;
+
+import com.ssafy.backend.common.dto.response.CommonResponse;
+import com.ssafy.backend.security.dto.JwtUserInfo;
+import com.ssafy.backend.user.dto.request.ModifyProfileRequestDTO;
+import com.ssafy.backend.user.dto.response.LoginResponseDTO;
+import com.ssafy.backend.user.dto.response.ModifyProfileResponseDTO;
+import com.ssafy.backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    @PutMapping("/{userId}")
+    public CommonResponse<ModifyProfileResponseDTO> modifyProfile(
+//            @AuthenticationPrincipal JwtUserInfo jwtUserInfo,
+            @PathVariable Long userId,
+            @RequestPart ModifyProfileRequestDTO modifyProfileRequestDTO, @RequestPart(value = "image",required = false) MultipartFile image) throws IOException {
+        ModifyProfileResponseDTO modifyProfile = userService.modifyProfile(
+//                jwtUserInfo.getUserId(),
+               userId,
+                modifyProfileRequestDTO,image);
+        return new CommonResponse<>(modifyProfile, HttpStatus.OK);
+    }
+}

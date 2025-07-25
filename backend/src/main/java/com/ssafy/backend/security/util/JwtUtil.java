6f70ff751a03f445bf5347dd026eec8945f713f0
강승구 <v010309@naver.com>
2025-07-25 14:04:44 +0900

    feat: deploy 프로필 추가

--- backend/src/main/java/com/ssafy/backend/security/util/JwtUtil.java
+++ backend/src/main/java/com/ssafy/backend/security/util/JwtUtil.java
@@ -12,6 +12,8 @@ import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -248,12 +250,7 @@ public class JwtUtil {
         return bearerToken;
     }
 
-    @PostConstruct
-    public void runOnceOnStartup() {
-        System.out.println("token cleanup scheduler initialized.");
-        cleanUpExpiredTokens(); // 스케줄러 로직 직접 호출
-    }
-
+    @Transactional
     @Scheduled(cron = "0 */5 * * * *") // 5분 마다 실행
     public void cleanUpExpiredTokens() {
         LocalDateTime now = LocalDateTime.now();

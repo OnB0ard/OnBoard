41aa3b2dc1b9e75e9e47863d058b93905912c346
이동하 <ssafy1342461@gmail.com>
2025-08-06 14:48:01 +0900

    fix : 계획방에서 사용자의 status 및 type까지 확인할 수 있도록 수정

--- backend/src/main/java/com/ssafy/backend/plan/dto/response/UserInformationResponseDTO.java
+++ backend/src/main/java/com/ssafy/backend/plan/dto/response/UserInformationResponseDTO.java
@@ -1,11 +1,13 @@
 package com.ssafy.backend.plan.dto.response;
 
 import com.ssafy.backend.user.entity.UserStatus;
+import com.ssafy.backend.user.entity.UserType;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-public class UserStatusResponseDTO {
+public class UserInformationResponseDTO {
     private UserStatus userStatus;
+    private UserType userType;
 }

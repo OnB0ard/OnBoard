680eaf6c237ea5ee16d0b705e72bc05c3b63d044
이동하 <ssafy1342461@gmail.com>
2025-08-12 20:28:09 +0900

    fix : deleteBookmark에서 plan내부 bookmark인지 확인하는 로직 추가

--- backend/src/main/java/com/ssafy/backend/plan/repository/BookmarkQueryRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/backend/plan/repository/BookmarkQueryRepositoryImpl.java
@@ -1,27 +1,39 @@
-    package com.ssafy.backend.plan.repository;
+package com.ssafy.backend.plan.repository;
 
-    import com.querydsl.jpa.impl.JPAQueryFactory;
-    import com.ssafy.backend.place.entity.QPlace;
-    import com.ssafy.backend.plan.entity.Bookmark;
-    import com.ssafy.backend.plan.entity.QBookmark;
-    import lombok.RequiredArgsConstructor;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.backend.place.entity.QPlace;
+import com.ssafy.backend.plan.entity.Bookmark;
+import com.ssafy.backend.plan.entity.QBookmark;
+import lombok.RequiredArgsConstructor;
 
-    import java.util.List;
+import java.util.List;
+import java.util.Optional;
 
-    @RequiredArgsConstructor
-    public class BookmarkQueryRepositoryImpl implements BookmarkQueryRepository {
+@RequiredArgsConstructor
+public class BookmarkQueryRepositoryImpl implements BookmarkQueryRepository {
 
-        private final JPAQueryFactory queryFactory;
+    private final JPAQueryFactory queryFactory;
 
-        public List<Bookmark> findBookmarksByPlanId(Long planId)
-        {
-            QBookmark bm = QBookmark.bookmark;
-            QPlace p = QPlace.place;
+    public List<Bookmark> findBookmarksByPlanId(Long planId)
+    {
+        QBookmark bm = QBookmark.bookmark;
+        QPlace p = QPlace.place;
 
-            return queryFactory
-                    .selectFrom(bm)
-                    .join(bm.place, p).fetchJoin()
-                    .where(bm.plan.planId.eq(planId))
-                    .fetch();
-        }
+        return queryFactory
+                .selectFrom(bm)
+                .join(bm.place, p).fetchJoin()
+                .where(bm.plan.planId.eq(planId))
+                .fetch();
     }
+
+    public Optional<Bookmark> findByBookmarkIdAndPlanId(Long bookmarkId, Long planId) {
+        QBookmark b = QBookmark.bookmark;
+
+        Bookmark bookmark = queryFactory.selectFrom(b)
+                .where(b.bookmarkId.eq(bookmarkId)
+                        .and(b.plan.planId.eq(planId)))
+                .fetchOne();
+
+        return Optional.ofNullable(bookmark);
+    }
+}

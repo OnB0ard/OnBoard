15116bc6367306d0d1b639ce70307e492e41424f
SSAFY <lsn5963@naver.com>
2025-08-12 11:18:42 +0900

    S13P11A504-186 fix: Double로 null허용하게 바꾸기

--- backend/src/main/java/com/ssafy/backend/whiteBoard/dto/request/ModifyWhiteBoardObjectRequestDTO.java
+++ backend/src/main/java/com/ssafy/backend/whiteBoard/dto/request/ModifyWhiteBoardObjectRequestDTO.java
@@ -10,11 +10,11 @@ import java.util.List;
 public class ModifyWhiteBoardObjectRequestDTO {
     // 공통 필드
     private String type;     // 예: CIRCLE, TEXT, PEN, ARROW 등
-    private double x;
-    private double y;
-    private double scaleX;
-    private double scaleY;
-    private double rotation;
+    private Double x;
+    private Double y;
+    private Double scaleX;
+    private Double scaleY;
+    private Double rotation;
 
     // 선택적 필드
     private List<Double> points; // 화살표나 펜 도형일 경우

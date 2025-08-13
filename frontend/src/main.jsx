235967daf3b411b5594aedb80d5bc99aece1ed58
YH-edenbro <hyun96227@gmail.com>
2025-08-13 18:34:38 +0900

    MyPage

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -20,10 +20,7 @@ if(!modalRoot){
 setAccessTokenGetter(() => useAuthStore.getState().accessToken);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* BrowserRouter : 브라우저의 현재 주소를 저장하고 감지하는 역할 */}
-  </StrictMode>,
 )
